eb87b814e49f970c70e5b121c5e30e39cdb70340
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-02 22:35:23 -0300

    Section 8, class 98

--- index.android.js
+++ index.android.js
@@ -13,35 +13,29 @@ import {
   Button
 } from 'react-native';
 
-class MeuComponente extends Component {
-    render() {
-        return (
-            <View>
-                <Text>{this.props.teste}</Text>
-            </View>
-        );
-    }
-}
+
 
 export default class game_jokenpo extends Component {
 
     constructor(props) {
         super(props);
-         this.state = { texto: 'Texto teste'};
+
+        this.state = { escolhaUsuario: '' };
     }
 
-    alteraTexto() {
-        this.setState({texto: 'Outra coisa'});
+    jokenpo(escolhaUsuario) {
+        this.setState({escolhaUsuario});
     }
 
     render() {
         return (
             <View>
-                <MeuComponente teste={this.state.texto} />
-                <Button
-                    title='Botão'
-                    onPress={() => this.alteraTexto()}
-                />
+                <Text>Escolha do Computador</Text>
+                <Text>Escolha do Usuário {this.state.escolhaUsuario}</Text>
+                <Text>Resultado</Text>
+                <Button title='pedra' onPress={ () => this.jokenpo('pedra') } />
+                <Button title='papel' onPress={ () => this.jokenpo('papel') } />
+                <Button title='tesoura' onPress={ () => this.jokenpo('tesoura') } />
             </View>
         );
     }
