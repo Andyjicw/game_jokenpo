128646581959d186fc60d49aafa002b65ee55835
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-02 22:14:46 -0300

    Section 8, class 96

--- index.android.js
+++ index.android.js
@@ -12,42 +12,27 @@ import {
   View
 } from 'react-native';
 
+class MeuComponente extends Component {
+    render() {
+        return (
+            <View>
+                <Text>{this.props.fruta1} Parâmetro</Text>
+                <Text>{this.props.fruta2} Parâmetro</Text>
+            </View>
+        );
+    }
+}
+
 export default class game_jokenpo extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+        <MeuComponente
+            fruta1='Banana'
+            fruta2='Mamão'
+        />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('game_jokenpo', () => game_jokenpo);
